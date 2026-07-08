2a20d5b0aeadd3d889bae1ce3ad486990f786c31
Siddartha51sudo <ammikasiddartha@gmil.com>
2026-07-08 13:08:51 +0530

    i am connecting nats

--- main.go
+++ main.go
@@ -1,36 +1,43 @@
 package main
 
 import (
-	"strings"
-	"telemetry-collector/services"
 	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+	"telemetry-collector/services"
 	"time"
 
 	"telemetry-collector/db"
 	"telemetry-collector/models"
+
+	"github.com/nats-io/nats.go"
 )
 
 func main() {
 
-	// STEP 1: Get data from Health Endpoint
+	// STEP 1: Get data from Health form nats
 
-	resp, err := http.Get("http://localhost:5000/health")
+	nc, err := nats.Connect("nats://nats:4222")
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
+	defer nc.Close()
+	_, err = nc.Subscribe("infrastructure.health", func(msg *nats.Msg) {
 
-	var health models.Health
+		var health models.Health
 
-	err = json.NewDecoder(resp.Body).Decode(&health)
-	if err != nil {
-		panic(err)
-	}
-	fmt.PrintLn("jai ballaya")
+		err := json.Unmarshal(msg.Data, &health)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		ProcessHealth(health)
+	})
+
 	fmt.Println("Health Endpoint Data")
 	fmt.Println("-----------------------------")
 	fmt.Println("Status    :", health.Status)
@@ -40,10 +47,9 @@ func main() {
 	fmt.Println()
 
 	health.Service = strings.TrimSpace(strings.ToLower(health.Service))
-health.Status = strings.TrimSpace(strings.ToLower(health.Status))
+	health.Status = strings.TrimSpace(strings.ToLower(health.Status))
 
 	// STEP 2: Read events.json
-	
 
 	data, err := os.ReadFile("events.json")
 	if err != nil {
@@ -61,33 +67,11 @@ health.Status = strings.TrimSpace(strings.ToLower(health.Status))
 
 	// STEP 3: Find matching event
 
-
 	var matched *models.TelemetryEvent
 
 	for i := range eventFile.Events {
 
 		event := &eventFile.Events[i]
-		fmt.Println("Before Normalization")
-fmt.Println("Service      :", event.Payload.Service)
-fmt.Println("Failure Type :", event.Payload.FailureType)
-fmt.Println("Status       :", event.Payload.ServiceStatus)
-fmt.Printf("CPU Usage    : %.6f\n", event.Payload.CPUUsage)
-fmt.Printf("Memory Usage : %.6f\n", event.Payload.MemoryUsage)
-fmt.Printf("ResponseTime : %.6f\n", event.Payload.ResponseTime)
-fmt.Println("Error Count  :", event.Payload.ErrorCount)
-fmt.Println()
-
-		services.NormalizeTelemetry(&event.Payload)
-
-		fmt.Println("After Normalization")
-fmt.Println("Service      :", event.Payload.Service)
-fmt.Println("Failure Type :", event.Payload.FailureType)
-fmt.Println("Status       :", event.Payload.ServiceStatus)
-fmt.Printf("CPU Usage    : %.2f\n", event.Payload.CPUUsage)
-fmt.Printf("Memory Usage : %.2f\n", event.Payload.MemoryUsage)
-fmt.Printf("ResponseTime : %.2f\n", event.Payload.ResponseTime)
-fmt.Println("Error Count  :", event.Payload.ErrorCount)
-fmt.Println()
 
 		if event.Payload.Service == health.Service &&
 			event.Payload.ServiceStatus == health.Status {
@@ -101,8 +85,19 @@ fmt.Println()
 		fmt.Println("No matching event found.")
 		return
 	}
-	
-	
+	fmt.Println("Before Normalization")
+	fmt.Println("Service      :", matched.Payload.Service)
+	fmt.Println("Failure Type :", matched.Payload.FailureType)
+	fmt.Println("Status       :", matched.Payload.ServiceStatus)
+	fmt.Printf("CPU Usage    : %.6f\n", matched.Payload.CPUUsage)
+	fmt.Printf("Memory Usage : %.6f\n", matched.Payload.MemoryUsage)
+	fmt.Printf("ResponseTime : %.6f\n", matched.Payload.ResponseTime)
+	fmt.Println("Error Count  :", matched.Payload.ErrorCount)
+	fmt.Println()
+
+	services.NormalizeTelemetry(&matched.Payload)
+
+	fmt.Println("After Normalization")
 
 	fmt.Println()
 	fmt.Println("Matched Event")
@@ -117,7 +112,6 @@ fmt.Println()
 	fmt.Println("Status        :", matched.Payload.ServiceStatus)
 
 	// STEP 4: Convert Timestamp
-
 
 	var eventTime time.Time
 
@@ -135,29 +129,23 @@ fmt.Println()
 		}
 	}
 
+	// Paste the validation code HERE
 
-// Paste the validation code HERE
+	telemetry := models.Telemetry{
+		ServiceName:   matched.Payload.Service,
+		CPUUsage:      matched.Payload.CPUUsage,
+		MemoryUsage:   matched.Payload.MemoryUsage,
+		ResponseTime:  float64(matched.Payload.ResponseTime),
+		ServiceStatus: matched.Payload.ServiceStatus == "up",
+		Timestamp:     eventTime,
+	}
 
-
-telemetry := models.Telemetry{
-    ServiceName:   matched.Payload.Service,
-    CPUUsage:      matched.Payload.CPUUsage,
-    MemoryUsage:   matched.Payload.MemoryUsage,
-    ResponseTime:  float64(matched.Payload.ResponseTime),
-    ServiceStatus: matched.Payload.ServiceStatus == "up",
-    Timestamp:     eventTime,
-}
-
-err = services.ValidateTelemetry(telemetry)
-if err != nil {
-    panic(err)
-}
-
-
-
+	err = services.ValidateTelemetry(telemetry)
+	if err != nil {
+		panic(err)
+	}
 
 	// STEP 5: Connect PostgreSQL
-	
 
 	conn, err := db.Connect()
 	if err != nil {
@@ -168,9 +156,7 @@ if err != nil {
 	fmt.Println()
 	fmt.Println("Connected to PostgreSQL")
 
-	
 	// STEP 6: Insert into Database
-	
 
 	_, err = conn.Exec(
 		context.Background(),
@@ -214,104 +200,104 @@ if err != nil {
 	fmt.Println()
 	fmt.Println("Telemetry inserted successfully!")
 
-log := models.Log{
-	ServiceName: matched.Payload.Service,
-	LogLevel:    "INFO",
-	Message:     "Telemetry inserted successfully",
-	FailureType: matched.Payload.FailureType,
-	EventTime:   eventTime,
-}
+	log := models.Log{
+		ServiceName: matched.Payload.Service,
+		LogLevel:    "INFO",
+		Message:     "Telemetry inserted successfully",
+		FailureType: matched.Payload.FailureType,
+		EventTime:   eventTime,
+	}
 
-err = services.InsertLog(log)
-if err != nil {
-	fmt.Println("Failed to insert log:", err)
-} else {
-	fmt.Println("Log inserted successfully!")
-}
-fmt.Println("\nMetrics Table:")
+	err = services.InsertLog(log)
+	if err != nil {
+		fmt.Println("Failed to insert log:", err)
+	} else {
+		fmt.Println("Log inserted successfully!")
+	}
+	fmt.Println("\nMetrics Table:")
 
-rows, err := conn.Query(context.Background(), "SELECT * FROM telemetry")
-if err != nil {
-	panic(err)
-}
-defer rows.Close()
-fmt.Println("\n================ METRICS TABLE================")
-fmt.Printf("%-10s %-15s %-8s %-8s %-8s %-8s %-10s\n",
-    "EventID", "Service", "CPU", "Memory", "Resp", "Errors", "Status")
-fmt.Println("--------------------------------------------------------------------------")
-for rows.Next() {
-	var eventID, eventType, source, correlationID string
-	var timestamp time.Time
-	var failureType, service, status string
-	var cpuUsage, memoryUsage, responseTime float64
-	var errorCount int
-
-	err = rows.Scan(
-		&eventID,
-		&eventType,
-		&source,
-		&correlationID,
-		&timestamp,
-		&failureType,
-		&service,
-		&cpuUsage,
-		&memoryUsage,
-		&responseTime,
-		&errorCount,
-		&status,
-	)
-
+	rows, err := conn.Query(context.Background(), "SELECT * FROM telemetry")
 	if err != nil {
 		panic(err)
 	}
+	defer rows.Close()
+	fmt.Println("\n================ METRICS TABLE================")
+	fmt.Printf("%-10s %-15s %-8s %-8s %-8s %-8s %-10s\n",
+		"EventID", "Service", "CPU", "Memory", "Resp", "Errors", "Status")
+	fmt.Println("--------------------------------------------------------------------------")
+	for rows.Next() {
+		var eventID, eventType, source, correlationID string
+		var timestamp time.Time
+		var failureType, service, status string
+		var cpuUsage, memoryUsage, responseTime float64
+		var errorCount int
 
-	fmt.Printf("%-10s %-15s %-8.2f %-8.2f %-8.2f %-8d %-10s\n",
-    eventID,
-    service,
-    cpuUsage,
-    memoryUsage,
-    responseTime,
-    errorCount,
-    status,
-)
-}
+		err = rows.Scan(
+			&eventID,
+			&eventType,
+			&source,
+			&correlationID,
+			&timestamp,
+			&failureType,
+			&service,
+			&cpuUsage,
+			&memoryUsage,
+			&responseTime,
+			&errorCount,
+			&status,
+		)
 
-fmt.Println("\nLogs Table:")
+		if err != nil {
+			panic(err)
+		}
 
-rows2, err := conn.Query(context.Background(), "SELECT * FROM logs")
-if err != nil {
-	panic(err)
-}
-defer rows2.Close()
-fmt.Println("\n================== LOGS TABLE ==================")
-fmt.Printf("%-4s %-18s %-8s %-35s %-18s %-20s\n",
-    "ID", "Service", "Level", "Message", "Failure Type", "Event Time")
-fmt.Println("---------------------------------------------------------------------------------------------------------------")
-for rows2.Next() {
-	var id int
-	var serviceName, logLevel, message, failureType string
-	var eventTime time.Time
+		fmt.Printf("%-10s %-15s %-8.2f %-8.2f %-8.2f %-8d %-10s\n",
+			eventID,
+			service,
+			cpuUsage,
+			memoryUsage,
+			responseTime,
+			errorCount,
+			status,
+		)
+	}
 
-	err = rows2.Scan(
-		&id,
-		&serviceName,
-		&logLevel,
-		&message,
-		&failureType,
-		&eventTime,
-	)
+	fmt.Println("\nLogs Table:")
 
+	rows2, err := conn.Query(context.Background(), "SELECT * FROM logs")
 	if err != nil {
 		panic(err)
 	}
+	defer rows2.Close()
+	fmt.Println("\n================== LOGS TABLE ==================")
+	fmt.Printf("%-4s %-18s %-8s %-35s %-18s %-20s\n",
+		"ID", "Service", "Level", "Message", "Failure Type", "Event Time")
+	fmt.Println("---------------------------------------------------------------------------------------------------------------")
+	for rows2.Next() {
+		var id int
+		var serviceName, logLevel, message, failureType string
+		var eventTime time.Time
 
-	fmt.Printf("%-4d %-18s %-8s %-35s %-18s %-20s\n",
-    id,
-    serviceName,
-    logLevel,
-    message,
-    failureType,
-    eventTime.Format("2006-01-02 15:04:05"),
-)
-}
+		err = rows2.Scan(
+			&id,
+			&serviceName,
+			&logLevel,
+			&message,
+			&failureType,
+			&eventTime,
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%-4d %-18s %-8s %-35s %-18s %-20s\n",
+			id,
+			serviceName,
+			logLevel,
+			message,
+			failureType,
+			eventTime.Format("2006-01-02 15:04:05"),
+		)
+	}
 }
